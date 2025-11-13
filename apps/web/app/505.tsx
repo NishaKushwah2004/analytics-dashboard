126694e8cf2f450d2753721f774fce7b549cfc3c
NishaKushwah2004 <2004nishakushwah@gmail.com>
2025-11-13 09:38:29 +0530

    Initial commit: analytics dashboard monorepo

---
+++ apps/web/app/505.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ServerError() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-xl font-bold">Something went wrong</h1>
+    </div>
+  );
+}
