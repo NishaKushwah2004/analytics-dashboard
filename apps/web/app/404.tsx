6cfb96b165cf73f7753f1e5266f7956d0d60ee59
NishaKushwah2004 <2004nishakushwah@gmail.com>
2025-11-13 09:59:34 +0530

    Initial commit: analytics dashboard monorepo

--- apps/web/app/404.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function NotFound() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-xl font-bold">Page Not Found</h1>
-    </div>
-  );
-}
--- apps/web/app/500.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function ServerError() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-xl font-bold">Something went wrong</h1>
-    </div>
-  );
-}
